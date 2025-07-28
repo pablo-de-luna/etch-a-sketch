cefd87ea3351735c7fc10f4dcd1ead421586b3c4
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-07-28 13:03:58 -0600

    Add function that create grid squares inside div

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 "use strict";
 
+const canvas = document.querySelector("#grid-container");
+
+const gridSize = 16;
+const gridArea = gridSize * gridSize;
+
+const createSquares = () => {
+    for (let i = 0; i < gridArea; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "square")
+        canvas.appendChild(square);
+    }
+};
+
+createSquares();
