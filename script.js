f8966032a23981c22bbf8fdcffcde2a8a787ca93
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-07-30 13:42:20 -0600

    Add button and function to change grid size
    Refactored script.js

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 "use strict";
 
-const canvas = document.querySelector("#grid-container");
-const gridSize = 40;
-const gridArea = gridSize * gridSize;
-const spaceSize = 100 / gridSize;
+const restartButton = document.querySelector("#restart-button");
+const grid = document.querySelector("#grid-container");
 
-// Change the flex basis percentage of .space selector in CSS file
-document.documentElement.style.cssText =
-`--space-basis-percentage: ${spaceSize}%;`
+let gridSize = 10;
+let gridArea = gridSize * gridSize;
+
+const setSpaceSize = () => {
+    let spaceSize = 100 / gridSize;
+    document.documentElement.style.cssText =
+        `--space-basis-percentage: ${spaceSize}%;`
+};
 
 const createGrid = () => {
+    setSpaceSize();
     for (let i = 0; i < gridArea; i++) {
         const space = document.createElement("div");
         space.setAttribute("class", "space")
-        canvas.appendChild(space);
+        grid.appendChild(space);
     }
 };
 createGrid();
 
-const spaces = document.querySelectorAll("div.space");
+const changeSize = () => {
+    restartButton.addEventListener("click", () => {
+        for (let i = 0; i < gridArea; i++) {
+            grid.firstChild.remove();
+        }
+        gridSize = prompt("grid size", 10);
+        gridArea = gridSize * gridSize;
+        createGrid();
+        paintSpaceBlack();
+    });
+};
+changeSize();
 
 const paintSpaceBlack = () => {
+    const spaces = document.querySelectorAll("div.space");
+
     spaces.forEach(space => space.addEventListener("mouseover", () => {
         space.setAttribute("style", "background: black;");
     }));
